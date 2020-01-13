8042d732469f9a90ba915a8db65d657954ff2a2a
Alyssa Gao <alyssa.gao@yahoo.ca>
2020-01-12 22:23:57 -0500

    Initial commit.

---
+++ Freshqo/src/main/java/com/alyssa/Freshqo/FoodQuantityDialog.java
@@ -0,0 +1,121 @@
+package com.alyssa.Freshqo;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSpinner;
+import javax.swing.SpinnerModel;
+import javax.swing.SpinnerNumberModel;
+
+/**
+ * FoodQuantityDialog 
+ * 
+ * The Dialog used to select food quantity
+ * 
+ * @author Alyssa Gao && Zaid Omer
+ * @version 1.0
+ * @date June 13, 2019
+ */
+public class FoodQuantityDialog extends JDialog {
+
+	// VARIABLES
+	private TableOrderItem tableOrderItem;
+	private TableOrder tableOrder;
+	private JPanel panel;
+	private ImageIcon background;
+
+	private JSpinner quantitySpinner;
+	private JButton orderButton;
+
+	/**
+	 * FoodQuantityDialog constructor
+	 * 
+	 * @param menuItem   the menu item ordered
+	 * @param waiter     the waiter serving this order
+	 * @param tableOrder the table order variable
+	 */
+	public FoodQuantityDialog(MenuItem menuItem, Waiter waiter, TableOrder tableOrder) {
+		this.tableOrderItem = new TableOrderItem(menuItem, waiter, tableOrder);
+		this.tableOrder = tableOrder;
+		initUI();
+	}
+
+	/**
+	 * initializes the user interface
+	 */
+	private void initUI() {
+
+		setModalityType(ModalityType.APPLICATION_MODAL);
+
+		setUndecorated(false); // TODO change to true
+		setSize(200, 300);
+		setLocationRelativeTo(null);
+		setResizable(false);
+
+		panel = new JPanel();
+		panel.setLayout(null);
+		getContentPane().add(panel);
+
+		JLabel quantityLabel = new JLabel("Quantity of Item: ");
+		quantityLabel.setBounds(10, 120, 125, 30);
+		panel.add(quantityLabel);
+
+		SpinnerModel quantitySpinnerModel = new SpinnerNumberModel(1, 1, 10, 1);
+		quantitySpinner = new JSpinner(quantitySpinnerModel);
+		quantitySpinner.setBounds(135, 120, 50, 30);
+		panel.add(quantitySpinner);
+
+		orderButton = new JButton(new ImageIcon(getClass().getResource("order button.JPG")));
+		orderButton.setBounds(40, 200, 120, 50);
+		panel.add(orderButton);
+		orderButton.addActionListener(new ActionListener() {
+			@Override
+			/**
+			 * actionPerformed Invoked when an action occurs
+			 *
+			 * @param press the action that occurs
+			 */
+			public void actionPerformed(ActionEvent press) {
+
+				if (press.getSource() == orderButton) {
+					int quantity = (int) quantitySpinner.getValue();
+					// checks if item has been ordered before and not fired
+					// if so, add to previous item and update quantity
+
+					boolean orderBefore = false;
+					for (int i = 0; i < tableOrder.getOrderItems().size(); i++) {
+						if ((tableOrder.getOrderItems().get(i).getMenuItem().equals(tableOrderItem.getMenuItem()))
+								&& (!tableOrder.getOrderItems().get(i).isFired())) {
+							tableOrder.getOrderItems().get(i)
+									.setQuantity(tableOrder.getOrderItems().get(i).getQuantity() + quantity);
+							orderBefore = true;
+						}
+					}
+					if (!orderBefore) {
+						tableOrderItem.setQuantity(quantity);
+						tableOrder.getOrderItems().add(tableOrderItem);
+					}
+					tableOrder
+							.setSubtotal(tableOrder.getSubtotal() + quantity * tableOrderItem.getMenuItem().getPrice());
+					tableOrder.setTotal(tableOrder.getSubtotal() * 1.13);
+				}
+				dispose();
+
+			}
+		});
+
+		// background image
+		background = new ImageIcon(getClass().getResource("small dialog background.JPG"));
+		JLabel backgroundLabel = new JLabel(background);
+		backgroundLabel.setBounds(0, 0, 200, 300);
+		panel.add(backgroundLabel);
+
+		setVisible(true);
+	}
+
+}
---
+++ Freshqo/src/main/java/com/alyssa/Freshqo/Utils.java
@@ -0,0 +1,85 @@
+package com.alyssa.Freshqo;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Utils
+ * 
+ * Formats time and date
+ * 
+ * @author Alyssa Gao
+ * @version 1.0
+ * @date June 13, 2019
+ */
+
+public class Utils {
+
+	// Variables
+	public static String DateFormatPattern = "MMMM d, yyyy";
+	public static String TimeFormatPattern = "h:ma";
+
+	/**
+	 * gets the date formatted
+	 * 
+	 * @return the date formatted
+	 */
+	public static DateTimeFormatter getDateFormatter() {
+		return DateTimeFormatter.ofPattern(DateFormatPattern);
+	}
+
+	/**
+	 * gets the list of dates formatted
+	 * 
+	 * @return the list of dates formatted
+	 */
+	public static ArrayList<DateTimeFormatter> getDateFormatters() {
+		ArrayList<DateTimeFormatter> dateFormatters = new ArrayList<>();
+		dateFormatters.add(getDateFormatter());
+		return dateFormatters;
+	}
+
+	/**
+	 * gets the time formatted
+	 * 
+	 * @return the time formatted
+	 */
+	public static DateTimeFormatter getTimeFormatter() {
+		return DateTimeFormatter.ofPattern(TimeFormatPattern);
+	}
+
+	/**
+	 * gets the list of times formatted
+	 * 
+	 * @return the list of times formatted
+	 */
+	public static List<DateTimeFormatter> getTimeFormatters() {
+		List<DateTimeFormatter> dateFormatters = new ArrayList<>();
+		dateFormatters.add(getTimeFormatter());
+		return dateFormatters;
+	}
+
+	/**
+	 * converts a string to local date
+	 * 
+	 * @param dateString the date in string format
+	 * @return the date in LocalDate format
+	 */
+	public static LocalDate convertToLocalDate(String dateString) {
+		return LocalDate.parse(dateString, getDateFormatter());
+	}
+
+	/**
+	 * converts a string to local time
+	 * 
+	 * @param timeString the date in string format
+	 * @return the time in LocalTime format
+	 */
+	public static LocalTime convertToLocalTime(String timeString) {
+		return LocalTime.parse(timeString.toUpperCase(), DateTimeFormatter.ofPattern(TimeFormatPattern));
+	}
+
+}
